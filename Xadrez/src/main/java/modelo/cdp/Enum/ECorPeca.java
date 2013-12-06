ace50a331b7daba43f900c8ec9889873199ece8c
DNascimento69 <dbnascimentto@gmail.com>
2013-12-06 11:49:05 -0200

    Peças

---
+++ Xadrez/src/main/java/modelo/cdp/Enum/ECorPeca.java
@@ -0,0 +1,19 @@
+package modelo.cdp.Enum;
+
+public enum ECorPeca 
+{
+	BRANCO("Branco"),
+	PRETO("Preto");
+
+	private String nome;
+	
+	ECorPeca(String n)
+	{
+		this.nome = n;
+	}
+	
+	public String getNome()
+	{
+		return this.nome;
+	}
+}
---
+++ Xadrez/src/main/java/modelo/cdp/pecas/Torre.java
@@ -0,0 +1,103 @@
+package modelo.cdp.pecas;
+
+import java.util.ArrayList;
+
+import modelo.cdp.Posicao;
+import modelo.cdp.Tabuleiro;
+import modelo.cdp.Enum.ECorPeca;
+
+public class Torre extends Peca
+{
+	public Torre(int x, int y, ECorPeca c, String n)
+	{
+		this.setPosicao(new Posicao(x, y));
+		this.setCor(c);
+		this.setNome(n);
+	}
+
+	// esta função retorna TODAS as possibilidades de destinos da Torre, incluindo as que ela pode comer alguma peça
+	public ArrayList<Posicao> criaListaDestinosPossiveis(Tabuleiro tabuleiro)
+	{
+		// a Torre pode andar para Norte, Sul, Leste e Oeste ateh o fim do tabuleiro ou ateh encontrar uma peça
+		
+		ArrayList<Posicao> posicoes = new ArrayList<Posicao>();
+		
+		// Norte
+		int buscaY = this.getPosicao().getY() + 1;
+		while (buscaY <= 7)
+		{
+			Posicao novaPos = new Posicao(this.getPosicao().getX(), buscaY);
+			Peca pecaPos = Tabuleiro.temPeca(tabuleiro, novaPos);
+			
+			if(pecaPos != null) // verifica se tem alguma peça na posicao de destino
+			{
+				if(this.getCor() != pecaPos.getCor()) // verifica se a peça da posiçao de destino pode ser comida pela que esta sendo usada
+					posicoes.add(novaPos);
+				break; // sai do loop pois a peça nao pode andar mais que este ponto
+			} else {
+				posicoes.add(novaPos);
+			}
+			
+			buscaY++;
+		}
+		
+		// Sul
+		buscaY = this.getPosicao().getY() - 1;
+		while (buscaY >= 0)
+		{
+			Posicao novaPos = new Posicao(this.getPosicao().getX(), buscaY);
+			Peca pecaPos = Tabuleiro.temPeca(tabuleiro, novaPos);
+			
+			if(pecaPos != null) // verifica se tem alguma peça na posicao de destino
+			{
+				if(this.getCor() != pecaPos.getCor()) // verifica se a peça da posiçao de destino pode ser comida pela que esta sendo usada
+					posicoes.add(novaPos);
+				break; // sai do loop pois a peça nao pode andar mais que este ponto
+			} else {
+				posicoes.add(novaPos);
+			}
+			
+			buscaY--;
+		}
+		
+		// Leste
+		int buscaX = this.getPosicao().getX() + 1;
+		while (buscaX <= 7)
+		{
+			Posicao novaPos = new Posicao(buscaX, this.getPosicao().getY());
+			Peca pecaPos = Tabuleiro.temPeca(tabuleiro, novaPos);
+			
+			if(pecaPos != null) // verifica se tem alguma peça na posicao de destino
+			{
+				if(this.getCor() != pecaPos.getCor()) // verifica se a peça da posiçao de destino pode ser comida pela que esta sendo usada
+					posicoes.add(novaPos);
+				break; // sai do loop pois a peça nao pode andar mais que este ponto
+			} else {
+				posicoes.add(novaPos);
+			}
+			
+			buscaX++;
+		}
+		
+		// Oeste
+		buscaX = this.getPosicao().getX() - 1;
+		while (buscaX >= 0)
+		{
+			Posicao novaPos = new Posicao(buscaX, this.getPosicao().getY());
+			Peca pecaPos = Tabuleiro.temPeca(tabuleiro, novaPos);
+			
+			if(pecaPos != null) // verifica se tem alguma peça na posicao de destino
+			{
+				if(this.getCor() != pecaPos.getCor()) // verifica se a peça da posiçao de destino pode ser comida pela que esta sendo usada
+					posicoes.add(novaPos);
+				break; // sai do loop pois a peça nao pode andar mais que este ponto
+			} else {
+				posicoes.add(novaPos);
+			}
+			
+			buscaX--;
+		}
+		
+		return posicoes;
+	}
+}
